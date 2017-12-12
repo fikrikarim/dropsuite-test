e3647edbda13aa4ac2bdcb835b04e58862ffadbe
Fikri Karim <fkfikrikarim@gmail.com>
2017-12-12 10:11:16 +0700

    App now can take an argument as the directory to search

--- App.rb
+++ App.rb
@@ -1,3 +1,6 @@
-dir = Dir["**/*"]
+# Takes the first argument from the command line to get a specific location
+location = ARGV[0] || "."
+location = location + "/**/*"
 
+dir = Dir[location]
 puts dir
