542f4a76a634207fcaa8851644bbdfe1d28562a2
Fikri Karim <fkfikrikarim@gmail.com>
2017-12-12 11:21:38 +0700

    Now the app double check the files. After the App compares files using filesizes. Then app compares files with the same size using MD5

--- App.rb
+++ App.rb
@@ -1,8 +1,10 @@
+require 'digest/md5'
+
 # Takes the first argument from the command line to get a specific location
 location = ARGV[0] || "."
 location = location + "/**/*"
 
-files = {}
+hash1 = {}
 
 # Recursive search of the directory
 Dir[location].each do |filename|
@@ -11,22 +13,39 @@ Dir[location].each do |filename|
 
     # Store files with the same size on the same hash key
     size = File.size(filename)
-    if files.has_key? size
-        files[size].push filename
+    if hash1.has_key? size
+        hash1[size].push filename
     else
-        files[size] = [filename]
+        hash1[size] = [filename]
     end
 
 end
 
-# Array of list of identical files. Will return [key, value]
-identical_files = files.max_by{|key, value| value.length }
-number_of_files = identical_files[1].length
+# Array of files with same size. Will return [key, value]
+files_with_same_size = hash1.max_by{|key, value| value.length }
 
-# Read the content of the file
+# Check the files with identical size and compare their MD5 
+# to ensure the content is really the same
+hash2 = {}
+
+files_with_same_size[1].each do |filename|
+    key = Digest::MD5.hexdigest(IO.read(filename)).to_sym
+    if hash2.has_key? key
+        # puts "same file #{filename}"
+        hash2[key].push filename
+    else
+        hash2[key] = [filename]
+    end
+end
+
+# Array of files with identical MD5. Quite enough to search for identical files
+identical_files = hash2.max_by{|key, value| value.length }
+number_of_identical_files = identical_files[1].length
+
+# # Read the content of the file
 file = File.open(identical_files[1].first)
 content = file.read
 file.close
 
 # Print the content and the number of identical files
-puts "#{content} #{number_of_files}"
+puts "#{content} #{number_of_identical_files}"
