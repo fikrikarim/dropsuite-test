dde47ddb170b9493bdde2b339918e4c2cfaab250
Fikri Karim <fkfikrikarim@gmail.com>
2017-12-12 10:38:50 +0700

    Store files with the same size on the same hash key

--- App.rb
+++ App.rb
@@ -2,8 +2,19 @@
 location = ARGV[0] || "."
 location = location + "/**/*"
 
+files = {}
+
 Dir[location].each do |filename|
     # Skip directory
     next if File.directory?(filename)
-    puts filename
+
+    # Store files with the same size on the same hash key
+    size = File.size(filename)
+    if files.has_key? size
+        files[size].push filename
+    else
+        files[size] = [filename]
+    end
+
+    puts files
 end
